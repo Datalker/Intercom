08670476a2149f0987a9bc9300dbfbb07ceb7886
Evgeny (Mac) <e.arnautov@gmail.com>
2016-05-19 04:30:57 +0000

    csv on the fly

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from datetime import datetime
 from flask import Flask
 from flask import render_template
 from flask import url_for
-from flask import request
+from flask import request, make_response
 from flask import send_from_directory
+import flask
 import intercom
 
 app = Flask(__name__)
 
 result_file_path = './static/result.csv'
+csv_result = ''
 
 @app.route("/")
 def form():
@@ -20,22 +22,29 @@ def result():
     print("request.form = ", request.form )
 
     if request.method == 'POST':
+        global csv_result
         from_date = datetime.strptime(request.form['from_date'], '%Y.%m.%d')
         to_date = datetime.strptime(request.form['to_date'], '%Y.%m.%d')
         app_id = request.form['app_id']
         api_key = request.form['api_key']
         conv_parts = intercom.get_conversation_parts(from_date, to_date, app_id, api_key)
         conv_parts_dict = intercom.prepare_conv_parts(conv_parts, 'dict')
-        conv_parts_csv = intercom.prepare_conv_parts(conv_parts, 'csv')
-        fl = open(result_file_path, 'w')
-        fl.write(conv_parts_csv)
-        fl.close()
+        csv_result = intercom.prepare_conv_parts(conv_parts, 'csv')
+        # fl = open(result_file_path, 'w')
+        # fl.write(conv_parts_csv)
+        # fl.close()
         # print(conv_parts)
         return render_template('table.html', conv_parts=conv_parts_dict)
 
 @app.route('/csv')
 def download_file():
-    return send_from_directory(app.static_folder, 'result.csv', as_attachment=True)
+    response = make_response(csv_result)
+    # This is the key: Set the right header for the response
+    # to be downloaded, instead of just printed on the browser
+    response.headers["Content-Disposition"] = "attachment; filename=result.csv"
+    return response
+
+    # return send_from_directory(app.static_folder, 'result.csv', as_attachment=True)
 
 if __name__ == '__main__':
     app.debug = True
